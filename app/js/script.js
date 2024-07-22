43fa12b64adcbb605f7b7e22bf6ca4c109892135
avikeseal <avikeseal@gmail.com>
2024-07-22 17:18:39 -0400

    mobile menu setup complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll('.has-fade');
@@ -7,6 +8,7 @@ btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')) { //close hamburger menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         fadeElems.forEach(function(element) {
             element.classList.remove("fade-in");
@@ -16,6 +18,7 @@ btnHamburger.addEventListener('click', function(){
         overlay.classList.add("fade-out");
     }
     else { //open hamburger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
         fadeElems.forEach(function(element) {
             element.classList.remove("fade-out");
